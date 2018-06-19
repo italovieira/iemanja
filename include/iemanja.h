8b2acebfb41516b881b458aa26a7679bf76db440
Ítalo Vieira <italogv@hotmail.com>
2018-06-19 00:14:31 -0300

    Realiza a extração dos componentes [WIP]

--- include/iemanja.h
+++ include/iemanja.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include "pilha.h"
+#include "fila.h"
 
 class Iemanja
 {
@@ -38,6 +39,7 @@ public:
 	void codigo_erro_3();
 	void codigo_erro_4();
 	
+        Fila<std::string> * extracao_componentes();
 
 	//6 - extração das componentes internos
 	void retirar_espacamento();
